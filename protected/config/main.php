fb37027b0836863a97e177457b24c014b9c8f03b
Vital Ozierski <ozicoder@gmail.com>
2013-02-03 20:24:42 +0300

    initial commit

---
+++ protected/config/main.php
@@ -0,0 +1,52 @@
+<?php
+
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'Video Order',
+
+	'preload'=>array('log'),
+
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+
+	'modules'=>array(
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'admin',
+			'ipFilters'=>array('127.0.0.1','::1'),
+		)
+	),
+
+	'components'=>array(
+
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+
+        'db' => require(dirname(__FILE__) . '/db.php'),
+
+		'errorHandler'=>array(
+			'errorAction'=>'site/error',
+		),
+
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+			),
+		),
+	),
+
+	'params'=>array(
+	),
+);
