bba2d5faa12716ffb4e67729ca0e42cb63a6d8de
Vital Ozierski <ozicoder@gmail.com>
2013-02-10 14:36:53 +0300

    email site fix

--- protected/views/email/new_order.php
+++ protected/views/email/new_order.php
@@ -2,4 +2,4 @@
 
 <p>На сайте появился новый заказ №<?=$order->id?></p>
 
-<p>Ссылка: <a href="http://video.ozis.by/admin/orders/<?=$order->id?>">http://video.ozis.by/admin/orders/<?=$order->id?></a>
+<p>Ссылка: <a href="http://videoreels.ru/admin/orders/<?=$order->id?>">http://video.ozis.by/admin/orders/<?=$order->id?></a>
